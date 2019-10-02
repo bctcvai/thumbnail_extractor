d52bed9f6572ec991e25d4e84c591778e2cd58ac
Brian Tate <brian.tate@cvisionai.com>
2019-10-02 19:31:59 -0400

    Support exporting imagery to a different project

--- tator/teardown.py
+++ tator/teardown.py
@@ -5,14 +5,14 @@ import json
 import os
 import sys
 
-def uploadThumbnails(tator, thumbnail_type_id, directory):
+def uploadThumbnails(tator, dest_tator, thumbnail_type_id, directory):
     for dir_element in os.listdir(directory):
         full_path=os.path.join(directory, dir_element)
         localization_id=int(os.path.splitext(dir_element)[0])
-        media=tator.Media.byName(dir_element)
+        media=dest_tator.Media.byName(dir_element)
         if not media:
             md5=pytator.md5sum.md5_sum(full_path)
-            tator.Media.uploadFile(thumbnail_type_id,
+            dest_tator.Media.uploadFile(thumbnail_type_id,
                                full_path,
                                waitForTranscode=True,
                                progressBars=False,
@@ -39,10 +39,12 @@ if __name__ == '__main__':
         print("ERROR: No pipeline arguments specified!")
         sys.exit(-1)
     thumbnail_type_id = pipeline_args['imageTypeId']
+    dest_project_id = pipeline_args.get('destProject', project_id)
 
     tator = pytator.Tator(rest_svc, token, project_id)
+    dest_tator = pytator.Tator(rest_svc, token, dest_project_id)
 
     for dir_element in os.listdir(work_dir):
         full_path=os.path.join(work_dir, dir_element)
         if os.path.isdir(full_path):
-            uploadThumbnails(tator, thumbnail_type_id, full_path)
+            uploadThumbnails(tator, dest_tator, thumbnail_type_id, full_path)
