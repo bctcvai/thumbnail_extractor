24717ec4217216fe89ce156d5768f04c69284478
Brian Tate <brian.tate@cvisionai.com>
2019-10-03 05:21:11 -0400

    modify teardown to copy states/localizations for keyframe extractions

--- tator/teardown.py
+++ tator/teardown.py
@@ -8,10 +8,11 @@ import sys
 def uploadThumbnails(tator, dest_tator, mode, thumbnail_type_id, directory):
     for dir_element in os.listdir(directory):
         full_path=os.path.join(directory, dir_element)
-        localization_id=int(os.path.splitext(dir_element)[0])
-        media=dest_tator.Media.byName(dir_element)
+        original_media_id = os.path.basename(directory).split('_')[0]
+        localization_id_or_frame=int(os.path.splitext(dir_element)[0])
+        md5=pytator.md5sum.md5_sum(full_path)
+        media=dest_tator.Media.byMd5(md5)
         if not media:
-            md5=pytator.md5sum.md5_sum(full_path)
             dest_tator.Media.uploadFile(thumbnail_type_id,
                                full_path,
                                waitForTranscode=True,
@@ -19,11 +20,65 @@ def uploadThumbnails(tator, dest_tator, mode, thumbnail_type_id, directory):
                                md5=md5,
                                section="Thumbnails")
             media=tator.Media.byMd5(md5)
+        else:
+            print("Frame Extraction Media found in db.")
 
         if mode == "state" or "localization_keyframe":
-            #TODO: Duplicate state objects
+            frame = localization_id_or_frame
+            # Iterate over each state type that belongs to the destination
+            # image type.
+            state_types=dest_tator.StateType.filter({'media_id':media['id']})
+            if state_types is None:
+                state_types=[]
+            for state_type in state_types:
+                type_id = state_type['type']['id']
+                states=tator.State.get({'type': type_id,
+                                        'media' : original_media_id})
+                print(f"importing {state_type['type']['name']}")
+                for entry in states:
+                    if entry['frame'] != frame:
+                        continue
+                    obj={'media_ids': media['id'],
+                         'frame': 0, #images are always frame 0
+                         'type' : state_type['type']}
+                    obj.update(entry['attributes'])
+                    tator.State.new(obj)
+
+            #Clone types from localizations
+            localization_types=dest_tator.StateType.filter({'media_id':
+                                                            media['id']})
+            if localization_types is None:
+                localization_types=[]
+            for localization_type in localization_types:
+                type_id = localization_type['type']['id']
+                dtype = localization_type['type']['dtype']
+                print(f"importing {localization_type['type']['name']}")
+                localizations=tator.Localization.get({'type': type_id,
+                                                      'media' : original_media_id})
+                for entry in localizations:
+                    if entry['frame'] != frame:
+                        continue
+                    obj={'media_id': media['id'],
+                         'frame': 0, #images are always frame 0
+                         'type' : state_type['type']}
+                    obj.update(entry['attributes'])
+                    if dtype == 'box':
+                        obj['x'] = entry['x']
+                        obj['y'] = entry['y']
+                        obj['height'] = entry['height']
+                        obj['width'] = entry['width']
+                    elif dtype == 'line':
+                        obj['x0'] = entry['x0']
+                        obj['y0'] = entry['y0']
+                        obj['x1'] = entry['x1']
+                        obj['y1'] = entry['y1']
+                    elif dtype == 'dot':
+                        obj['x'] = entry['x']
+                        obj['y'] = entry['y']
+                    tator.Localization.new(obj)
             pass
         elif mode == "localization_thumbnail":
+            localization_id = localization_id_or_frame
             localization=tator.Localization.get(localization_id)
             media_attrs=media['attributes']
             media_attrs.update(localization['attributes'])
